17ddba7f1a03c53be2030fb59696c86a78a4bf33
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2026-02-20 22:45:10 -0300

    fix: change migration export

--- infra/migrations/1770681037175_test-migration.js
+++ infra/migrations/1770681037175_test-migration.js
@@ -1,18 +1,7 @@
-/**
- * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
- */
 export const shorthands = undefined;
-
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
- * @returns {Promise<void> | void}
- */
-export const up = (pgm) => {};
-
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
- * @returns {Promise<void> | void}
- */
-export const down = (pgm) => {};
+export function up(pgm) {
+  // UP
+}
+export function down(pgm) {
+  // DOWN
+}
