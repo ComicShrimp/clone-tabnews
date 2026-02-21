d217d3f48b115760c820e7a55b8786aba13f06e6
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2026-02-20 22:37:14 -0300

    feat: adds `/api/v1/migrations` endpoint

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+import nextJest from "next/jest.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: ".env.development",
+});
+
+const createJestConfig = nextJest({
+  dir: ".",
+});
+const jestConfig = createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>"],
+});
+
+export default jestConfig;
